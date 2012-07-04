0c75efa61ee9264de45f5a3a3d100fa813bc0c19
jslhs <jslhs0@gmailcom>
2012-07-04 22:57:26 +0800

    hello, git
    Signed-off-by: jslhs <jslhs0@gmailcom>

---
+++ src/hello.c
@@ -0,0 +1,15 @@
+/*
+ * hello.c
+ *
+ *  Created on: Jul 4, 2012
+ *      Author: snowcat
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("hello, git!!!\n");
+	return 0;
+}
